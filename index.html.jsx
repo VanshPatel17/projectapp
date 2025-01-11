8a1cd79a246eebdc0c83c3d2620da10f22c49ede
VanshPatel17 <126960277+VanshPatel17@users.noreply.github.com>
2025-01-11 12:52:50 +0530

    Add files via upload

---
+++ index.html.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Dessert from './Dessert'
+import Counter from './Counter'
+import Header from './Header'
+import Footer from './Footer'
+import Register from './Register'
+
+export default function App() {
+  return (
+    <div>{/*
+      <Header/>
+      <Dessert/>
+      <Counter/>
+      <Footer/>*/}
+      <Register/>
+    </div>
+  )
+}
